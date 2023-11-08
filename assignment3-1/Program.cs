9d0f8c35d284d361cbf1e79622b62b6f1d710e0a
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-08 21:25:57 +0530

    assignment3-1

---
+++ assignment3-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+               string input;
+
+                // Using TryParse for integer conversion
+                Console.Write("Enter an integer: ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out int intResult))
+                {
+                    Console.WriteLine("TryParse - Integer Conversion Successful. Result: " + intResult);
+                }
+                else
+                {
+                    Console.WriteLine("TryParse - Integer Conversion Failed. Invalid input.");
+                }
+
+                // Using Convert for double conversion
+                Console.Write("Enter a double: ");
+                input = Console.ReadLine();
+                try
+                {
+                    double doubleResult = Convert.ToDouble(input);
+                    Console.WriteLine("Convert - Double Conversion Successful. Result: " + doubleResult);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Convert - Double Conversion Failed. Format error.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Convert - Double Conversion Failed. Overflow error.");
+                }
+
+                // Using Parse for decimal conversion
+                Console.Write("Enter a decimal: ");
+                input = Console.ReadLine();
+                try
+                {
+                    decimal decimalResult = decimal.Parse(input);
+                    Console.WriteLine("Parse - Decimal Conversion Successful. Result: " + decimalResult);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Parse - Decimal Conversion Failed. Format error.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Parse - Decimal Conversion Failed. Overflow error.");
+                }
+            
+        }
+    }
+}
